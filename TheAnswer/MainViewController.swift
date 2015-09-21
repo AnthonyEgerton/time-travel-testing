ef02300fc1567589197e8dd1b2eca19670c3a77d
Anthony Egerton <anthony@delfish.com>
2015-09-21 23:40:35 +1000

    Add activityIndicator.

--- TheAnswer/MainViewController.swift
+++ TheAnswer/MainViewController.swift
@@ -2,8 +2,22 @@ import UIKit
 
 class MainViewController: UIViewController {
     @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var answerActivityIndicator: UIActivityIndicatorView!
     private let answerService = AnswerService()
-
+    
+    private enum State {
+        case Busy, Idle
+    }
+    
+    private var state: State = .Idle {
+        didSet {
+            switch state {
+            case .Busy: answerActivityIndicator.startAnimating()
+            case .Idle: answerActivityIndicator.stopAnimating()
+            }
+        }
+    }
+    
     @IBAction func tellMeTheAnswer(sender: UIButton) {
         let answer = answerService.whatIsTheAnswer()
         answerLabel.text = String(answer)
