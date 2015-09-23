97c5eaa5e6433b7b6bd5842e6e4b9de0d9f87f5c
Anthony Egerton <git@delfish.com>
2015-09-24 07:53:12 +1000

    Warning 1. Don't just add delays Adding the arbitrary delay to the test is a bad idea because it creates unreliable tests. The test sometimes works, sometimes doesn’t.

--- TheAnswerTests/AnswerServiceTests.swift
+++ TheAnswerTests/AnswerServiceTests.swift
@@ -6,9 +6,19 @@ class AnswerServiceTests: XCTestCase {
         var answer: Answer! = nil
         
         subject.whatIsTheAnswer { answer = $0 }
+        delay(4)//💣🕒‼️💥🔥
         
         XCTAssertEqual(String(answer), "42")
     }
 
     private let subject = AnswerService()
+}
+
+private func delay(seconds: NSTimeInterval) {
+    insertTimeBombThatWillBiteMeSomeTimeInTheFuture(seconds)
+}
+
+private func insertTimeBombThatWillBiteMeSomeTimeInTheFuture(seconds: NSTimeInterval) {
+    let date = NSDate(timeIntervalSinceNow: seconds)
+    NSRunLoop.mainRunLoop().runUntilDate(date)
 }
